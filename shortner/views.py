c1b40d555a2592e3291b75a3a47d9e7cd4de02a1
Shubhajit Das <shubhajitdas121@gmail.com>
2021-09-23 00:14:40 +0530

    fixed url validation

--- shortner/views.py
+++ shortner/views.py
@@ -1,22 +1,29 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render, redirect
+import validators
 import uuid
 from .models import Url
 from django.http import HttpResponse
+from django.contrib import messages
 
 
-# Create your views here.
 def index(request):
     return render(request, 'index.html')
 
-
 def shortenUrl(request):
-    if request.method == "POST":
-        url = request.POST['link']
-        uid = str(uuid.uuid4())[:5]
-        new_url = Url(link=url, uuid=uid)
-        new_url.save()
-        return HttpResponse(uid)
-
+        if request.method == "POST":
+            url = request.POST['link']
+        if validators.url(url):
+            if(Url.objects.filter(link=url).exists()):
+                uid = Url.objects.get(link=url).uuid
+            else:
+                uid = str(uuid.uuid4())[:5]
+                new_url = Url(link=url, uuid=uid)
+                new_url.save()
+            return HttpResponse(uid)
+        else:
+            messages.info(request, "Entered URL is not valid!")
+            return HttpResponseRedirect('index.html')
 
 def redirectUrl(request, hash_value):
     long_url_details = Url.objects.get(uuid=hash_value)
